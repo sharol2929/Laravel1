0708fc6ed1966106145f54287dc9717dda24818e
sharol2929 <sharol3222@gmail.com>
2023-05-28 16:12:20 -0500

    Creación de la vista Manga

--- entrega1/resources/views/sobre_media/manga.php
+++ entrega1/resources/views/sobre_media/manga.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="/">
                         <img
                             src="../imagenes/logo.png"
                             class="me-2"
@@ -58,13 +58,13 @@
                         <!-- Dropdown menu -->
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/personajes">Personajes</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/personajes">Personajes</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/organizaciones">Organizaciones</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/organizaciones">Organizaciones</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/akuma_no_mi">Akuma no mi</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/akuma_no_mi">Akuma no mi</a>
                                 </li>
                             </ul>
                         </li>
@@ -84,10 +84,10 @@
                             <!-- Dropdown menu -->
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li>
-                                    <a class="dropdown-item" href="sobre_media/manga">Manga</a>
+                                    <a class="dropdown-item" href="manga">Manga</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_media/anime">Anime</a>
+                                    <a class="dropdown-item" href="anime">Anime</a>
                                 </li>
                             </ul>
                        </li>
@@ -95,6 +95,218 @@
                 </div>
             </div>
         </nav><br>
+        <div class="col-md-8" id="divtabla">
+            <div class="hstack gap-3">
+                <div class="vstack gap-3">
+                    <h2>Sipnosis</h2>
+                    <hr class="hr">
+                    <p>One Piece es un manga y anime que narra las aventuras de una tripulación de piratas1. El protagonista es Monkey D. Luffy, un joven que quiere ser el Rey de los piratas y que tiene el poder de estirar su cuerpo como un caucho2. Para lograr su sueño, Luffy y sus amigos deben encontrar el One Piece, el tesoro más grande del mundo, que fue escondido por el anterior Rey de los piratas, Gol D. Roger</p>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th><img src="../imagenes/logo.png" class="me-2" height="150" alt="MDB Logo" loading="lazy"/></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nombre: ワンピース</td>
+                            </tr>
+                            <tr>
+                                <td>Creador: Eiichirō Oda</td>
+                            </tr>
+                            <tr>
+                                <td>Editorial: Shūeisha</td>
+                            </tr>
+                            <tr>
+                                <td>Revista: Shōnen Jump </td>
+                            </tr>
+                            <tr>
+                                <td>Fecha de inicio: 22 de julio de 1997</td>
+                            </tr>
+                            <tr>
+                                <td>Estado: En publicación</td>
+                            </tr>
+                            <tr>
+                                <td>Volumenes: 105</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>    
+            </div><br>
+            <h2>Volumenes Favoritos</h2><hr>
+            <div class="hstack gap-3">
+                <div class="vstack gap-3">
+                    
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen uno</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 24 de diciembre de 1997</th>
+                        </thead>
+                        <thead>
+                            <th>Título: Romance Amanecer</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v1.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Primer Volumen del manga</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen 12</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 02 de febrero de 2000</th>
+                        </thead>
+                        <thead>
+                            <th>Título: La leyenda inicia</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v2.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Empiezan su viaje en Grand Line</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen 16</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 04 de diciembre de 2000</th>
+                        </thead>
+                        <thead>
+                            <th>Título: Llevar a cabo su voluntad</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v3.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Presentan por primera vez a chopper</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="hstack gap-3">
+                <div class="vstack gap-3">
+                    
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen 23</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 04 de abril de 2002</th>
+                        </thead>
+                        <thead>
+                            <th>Título: La aventura de Vivi</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v4.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Finaliza batalla de Arabasta</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen 44</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 04 de diciembre de 2006</th>
+                        </thead>
+                        <thead>
+                            <th>Título: Volvamos</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v5.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Muerte del Going Merry</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th class="table-info">
+                                <h4>Volumen 57</h4>
+                            </th>    
+                        </thead>
+                        <thead>
+                            <th>Fecha: 04 de marzo de 2010</th>
+                        </thead>
+                        <thead>
+                            <th>Título: Guerra Paramount</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                <img src="../imagenes/volumen/v6.jpg" class="me-2" alt="MDB Logo" height="300px" loading="lazy"/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Queremos a salvar a Ace</label></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+
+
+
+
+
+
+
+    <!-- MDB -->
+    <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.js"
+    ></script>
+
+    <!-- B -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
