892fbcfe4353969fb3ed7cf520da1ebb12092682
sharol2929 <sharol3222@gmail.com>
2023-05-28 17:46:21 -0500

    Creación de la vista anime, y fin de proyecto.

--- entrega1/resources/views/sobre_media/anime.php
+++ entrega1/resources/views/sobre_media/anime.php
@@ -32,7 +32,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="/">
                         <img
                             src="../imagenes/logo.png"
                             class="me-2"
@@ -58,13 +58,13 @@
                         <!-- Dropdown menu -->
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/personajes">Personajes</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/personajes">Personajes</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/organizaciones">Organizaciones</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/organizaciones">Organizaciones</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_mundo/akuma_no_mi">Akuma no mi</a>
+                                    <a class="dropdown-item" href="../sobre_mundo/akuma_no_mi">Akuma no mi</a>
                                 </li>
                             </ul>
                         </li>
@@ -84,10 +84,10 @@
                             <!-- Dropdown menu -->
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li>
-                                    <a class="dropdown-item" href="sobre_media/manga">Manga</a>
+                                    <a class="dropdown-item" href="manga">Manga</a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item" href="sobre_media/anime">Anime</a>
+                                    <a class="dropdown-item" href="anime">Anime</a>
                                 </li>
                             </ul>
                        </li>
@@ -95,6 +95,248 @@
                 </div>
             </div>
         </nav><br>
+        <div class="col-md-8" id="divtabla">
+            <div class="hstack gap-3">
+                <div class="vstack gap-3">
+                    <h2>Sipnosis</h2>
+                    <hr class="hr">
+                    <p>One Piece es un manga y anime que narra las aventuras de una tripulación de piratas1. El protagonista es Monkey D. Luffy, un joven que quiere ser el Rey de los piratas y que tiene el poder de estirar su cuerpo como un caucho2. Para lograr su sueño, Luffy y sus amigos deben encontrar el One Piece, el tesoro más grande del mundo, que fue escondido por el anterior Rey de los piratas, Gol D. Roger</p>
+                </div>
+                <div class="vstack gap-3">
+                    <table class="table">
+                        <thead>
+                            <th><img src="../imagenes/logo.png" class="me-2" height="150" alt="MDB Logo" loading="lazy"/></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nombre: ワンピース</td>
+                            </tr>
+                            <tr>
+                                <td>Creador: Eiichirō Oda</td>
+                            </tr>
+                            <tr>
+                                <td>Estudio: Toei Animation</td>
+                            </tr>
+                            <tr>
+                                <td>Canal TV: Fuji TV </td>
+                            </tr>
+                            <tr>
+                                <td>Fecha de inicio: 20 de octubre de 1999</td>
+                            </tr>
+                            <tr>
+                                <td>Estado: En emisión</td>
+                            </tr>
+                            <tr>
+                                <td>Capítulos: 1063</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>    
+            </div>
+            <h2>Sagas del anime</h2><hr><br>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga del East Blue</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 001:</td>
+                        <td>¡Soy Luffy! ¡El hombre que se convertirá en el Rey Pirata!</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 061</td>
+                        <td>¡Final enojado! ¡Cruza la Línea Roja!</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>20 de octubre de 1999 - 7 de marzo de 2001</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Arabasta Saga</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 062:</td>
+                        <td>¿El primer obstáculo? Giant Whale Laboon Looks</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 130</td>
+                        <td>¡Cuidado con su aroma! ¡El séptimo es Nico Robin!</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>21 de marzo de 2001 - 27 de octubre de 2002</td>
+                    </tr>
+                    <tr>
+                        <td>Episodios relleno</td>
+                        <td>131, 132, 133, 134, 135</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga de la Isla del Cielo</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 144:</td>
+                        <td>¡El registro está tomado! ¡Rey de salvamento, Masira!</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 195</td>
+                        <td>¡Ahora nos dirigimos hacia el Mar Azul! Recuerdos de un final</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>9 de febrero de 2003 - 13 de junio de 2004</td>
+                    </tr>
+                    <tr>
+                        <td>Episodios relleno</td>
+                        <td>Arco de la Isla de la Cabra: 136-138, Arco de la isla Ruluka: 139-143, Arco G-8: 196-206</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga del East Blue</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 207:</td>
+                        <td>	¡Gran aventura en Long Ring Long Land</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 325</td>
+                        <td>¡La habilidad más malvada! ¡La oscuridad de Barbanegra ataca a Ace</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>31 de octubre de 2004 - 23 de septiembre de 2007</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga del Thriller Bark</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 337:</td>
+                        <td>¡Aventúrate en el Mar del Diablo! ¡El misterioso esqueleto flotando a través de la niebla</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 381</td>
+                        <td>¡Un nuevo compañero de tripulación! - El músico "Humming Brook"!</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>6 de enero de 2008 - 14 de diciembre de 2008</td>
+                    </tr>
+                    <tr>
+                        <td>Episidos Relleno</td>
+                        <td>Arco del cazador de hielo: 326-336 Arco de la isla Spa: 382-384</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga de guerra de cumbres</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 385:</td>
+                        <td>¡Llegando a la mitad de la Grand Line! ¡La Línea</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 516</td>
+                        <td>Comienza el entrenamiento de Luffy. Nos vemos 2 años después, en el lugar</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>18 de enero de 2009 - septiembre 25, 2011</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Dressrosa Saga</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 579:</td>
+                        <td>¡Aterrizaje! La isla ardiente, Punk Hazard</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 746</td>
+                        <td>Clash of the Mightiest — Raging Monsters of the New World</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>enero 6, 2013 - junio 19, 2016</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Whole Cake Island Saga</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 747:</td>
+                        <td>La Fortaleza de Plata — Las Grandes Aventuras de Luffy y Bart</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 889</td>
+                        <td>Finalmente, comienza: ¡el nivel lleno de conspiración!</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>junio 26, 2016 - junio 16, 2019</td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered ">
+                <thead>
+                    <th class="table-warning" colspan="2"><h3><strong>Saga de Wano</strong></h3></th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Primer Episodio 890:</td>
+                        <td>¡Marco! ¡El guardián del último recuerdo de Barbablanca,</td>
+                    </tr>
+                    <tr>
+                        <td>Último Episodio 1063</td>
+                        <td>¡Luffy acelera! ¡El punto de inflexión de una nueva era!</td>
+                    </tr>
+                    <tr>
+                        <td>Duración</td>
+                        <td>June 23, 2019 - 28 de mayo de 2023</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
+
+
+
+
+
+
+
+
+
+     <!-- MDB -->
+     <script
+    type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.1/mdb.min.js"
+    ></script>
+
+    <!-- B -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
